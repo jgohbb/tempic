71910b36182de68c060a9d986a97678b339ac5ec
Lewis Lockhart <lewis00001@gmail.com>
2019-12-04 21:01:47 -0700

    added rand bg js, and supporting html and css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,17 @@
 // main app js file //
+$(document).ready(function () {
 
-// converts temp C to F
-function calcFahrenheit(c) {
-    return (c * (9/5)) + 32;
-}
+    // selects and displays random background image for the .content section via .bg class
+    var bg_images = ['bg3.jpg', 'bg4.jpg', 'bg5.jpg'];
+    $(".bg").css({
+        'background-image': 'url(\"assets/images/' +
+            bg_images[Math.floor(Math.random() * bg_images.length)] + '\")'
+    });
 
+    // converts temp C to F
+    function calcFahrenheit(c) {
+        return (c * (9 / 5)) + 32;
+    }
+
+    // end of document.ready
+});
